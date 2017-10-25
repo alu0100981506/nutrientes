000a371a8b9cf409074319a66b08ccbf21c973e5
Pablo Martin Gonzalez <alu0100981506@ull.edu.es>
2017-10-25 15:48:03 +0100

    Failure/Error: expect(@prueba.to_s).to eq([Huevo Frito, 14.1, 0.0, 19.5]) SOLUCIONADO

--- lib/Nutrientes/Nutrientes_.rb
+++ lib/Nutrientes/Nutrientes_.rb
@@ -2,14 +2,19 @@ class Cnutrientes
 
 attr_reader :Nombre, :Proteinas, :Glucidos, :Lipidos
 
-def initialize(n, p, g, l)
+ def initialize(n, p, g, l)
  
 	@Nombre = n
 	@Proteinas = p
 	@Glucidos = g
 	@Lipidos = l
 
-end
+ end
 
+def to_s
+	
+	"[#{@Nombre}, #{@Proteinas}, #{@Glucidos}, #{@Lipidos}]"
+
+end
 
 end
