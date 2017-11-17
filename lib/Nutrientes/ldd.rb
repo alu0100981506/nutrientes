33b5a488e250ff7560bfa77a7c417d5aec596075
Th3Ent <pablou19@gmail.com>
2017-11-17 10:09:25 +0000

    Documentacion hecha

--- lib/Nutrientes/ldd.rb
+++ lib/Nutrientes/ldd.rb
@@ -1,45 +1,46 @@
 
 Node = Struct.new(:value, :next_, :prev)
-
+#Clase Lista doblemente enlazada
 class Ldd
 
-    include Enumerable
+   include Enumerable
 
-    
+   #Getters
    attr_reader :cabeza, :cola
    
+   #Inicializacion
    def initialize(v)
         @cabeza = Node.new(v, nil, nil)
         @cola = @cabeza
    end
    
-   
+   #Metodo para insertar un valor por detras
    def insertar_cola(v)
         aux = Node.new(v,nil,@cola)
         @cola.next_ = aux
         @cola = aux
    end
-    
+    #Metodo para insertar un valor por delane
     def insertar_cabeza(v)
         aux = Node.new(v,@cabeza,nil)
         @cabeza.prev = aux
         @cabeza = aux
     end
-    
+    #Metodo para extraer el ultimo valor
     def extraer_cabeza
        dummy = @cabeza.value 
        @cabeza = @cabeza.next_
        @cabeza.prev = nil
        return dummy
     end
-    
+    #Metodo para extraer el primer valor
     def extraer_cola
        dummy = @cola.value 
        @cola = @cola.prev
        @cola.next_ = nil
        return dummy
     end
-    
+    #Metodo para escribir de forma normalizada la lista
     def to_s
        cadena = "("
        dummy = @cabeza
@@ -51,7 +52,7 @@ class Ldd
        return cadena
     end
     
-    
+    #Metodo para recorrer la lista devolviendo el valor
     def each
        actual = @cabeza
        while actual != nil do
